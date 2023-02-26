6685cf02c130d1350464bec2bf83820cec935ce0
ThalesHenriq <125931825+ThalesHenriq@users.noreply.github.com>
2023-02-26 17:45:59 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const download = document.querySelector(".download");
+const dark = document.querySelector(".dark");
+const light = document.querySelector(".light");
+const qrContainer = document.querySelector(".qrCode");
+const qrText = document.querySelector(".qe-text");
+const shareBtn = document.querySelector(".share-btn");
+const sizes = document.querySelector(".sizes");
+
+dark.addEventListener("input", handleDarkColor);
+light.addEventListener("input", handleLightColor);
+qrText.addEventListener("input", handleQRText);
+sizes.addEventListener("change",handleSize);
+shareBtn.addEventListener("click", handleShare);
+
+const defaultUrl = "https://youtube.com";
+let colorLight = "#fff",
+     colorDark = "000",
+     text = defaultUrl,
+     size = 300;
+
+function handleDarkColor(e) {
+    colorDark = e.target.value;
+    generateQRCode();
+}
+
+function handleLightColor(e) {
+    colorLight = e.target.value;
+    generateQRCode();
+}
+
+function handleQRText(e) {
+    const value = e.target.value;
+    text = value;
+    if (!value) {
+        text = defaultUrl
+    }
+    generateQRCode();
+}
+
+async function generateQRCode(){
+    qrContainer.innerHTML = ""
+    new generateQRCode("qr-code",{
+        text,
+        height: size,
+        width: size,
+        colorLight,
+        colorDark,
+    });
+    download.herf = await resolveDataUrl();
+}
